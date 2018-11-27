266a318c27c62c546c26729d85318345ac06424c
Ken-S-0114 <g031n071@s.iwate-pu.ac.jp>
2018-11-27 18:39:05 +0900

    first commit

---
+++ XKCD/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  XKCD
+//
+//  Created by 佐藤賢 on 2018/11/27.
+//  Copyright © 2018 佐藤賢. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
